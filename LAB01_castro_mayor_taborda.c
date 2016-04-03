78db6b2ae7b65c67ca7a5d68ac0720c1a9c90e22
Alexander Taborda Acosta <alextab93@gmail.com>
2016-04-02 19:36:56 -0500

    Add new Tokens

--- LAB01_castro_mayor_taborda.c
+++ LAB01_castro_mayor_taborda.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-#include "LAB01_castro_mayor_taborda.h"
+#include "Tokens.h"
 
 extern int yylex();
 extern int yylineno;
 extern char* yytext;
 
-char *nombres[] = {NULL, " Id= ", " Punto-coma= ", " VOID ", " Parent-a= ", " Parent-c= ", 
-                        " Inicio= ", " Fin= ", " Leer= ", " Escribir= ", " MAIN ", 
+char *nombres[] = {NULL, " Id= ", " Punto-coma= ", " VOID", " Parent-a= ", " Parent-c= ", 
+                        " Inicio= ", "Fin= ", " Leer= ", " Escribir= ", "MAIN", 
                         " Coma= ", " Op-sum= ", " Op-sust= ", " Op-mult= ", 
                         " Op-div= ", " Op-exp= ", " Cte-entera= ", " Cte-real= ", 
-                        " Cte-cadena= ", " INT ", " FLOAT ", " CHAR ", " Menor-igual= ", 
+                        " Cte-cadena= ", " INT", " FLOAT", " CHAR", " Menor-igual= ", 
                         " Mayor-igual= ", " Menor= ", " Mayor= ", " Difente= ", " Op-asign= ", 
                         " Comparacion= ", " Op-y= ", " Op-o= ", " Op-no= ", " Si= ", " Sino= ", 
                         " Para= ", " Mientras-que= ", " Haga-hasta= ", " Dependiendo-de= ", 
@@ -17,16 +17,25 @@ char *nombres[] = {NULL, " Id= ", " Punto-coma= ", " VOID ", " Parent-a= ", " Pa
 
 int main(void){
 
-  int ntoken, vtoken;
+  int ntoken;
 
   ntoken = yylex();
 
+  FILE *archivo = fopen("salida.txt", "w");
+
   while(ntoken){
     printf("%s", nombres[ntoken]);
-    printf("%s", yytext);
+    fprintf(archivo, nombres[ntoken]);
+    if(ntoken != 3 && ntoken != 10 && ntoken != 20 && ntoken != 21 && ntoken != 22){
+      printf("%s", yytext);
+      fprintf(archivo, yytext);
+    }
+    if(ntoken == 2 || ntoken == 6){
+      fprintf(archivo, "\n");
+    }
     ntoken = yylex();
   }
-
+  printf("%s\n", "");
+  fclose(archivo);
   return 0;
 }
-
