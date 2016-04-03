ad9447133b3d363f585b69e95735cbc9f84463fd
sammx343 <naitsabes29@gmail.com>
2016-04-02 19:25:15 -0500

    New prueba.c with more text to probe

--- prueba.c
+++ prueba.c
@@ -5,10 +5,54 @@ main (void)
   char c;
   char cadena;
   float z;
+  int 89aa12
   z=14.9e-8;
   z=12.9;
   cadena="Hola";
   scanf ("%d",i);
   i=i*2;
   printf ("El doble es %d",i);
+  Y="Cualquier Cosa 1";
+  u=z+y
+  "Una vaca vestida de uniforme"
+  p = "Una vaca vestida de uniforme";
+  @@
+  for(i=1;i<=13;i+=1){
+    if(i==13){
+      "Cole aqui fue";
+      cadena = "hola";
+      char muack = "muack";
+    }
+    printf("45");
+    switch(ntoken){
+      case TYPE:
+      case NAME:
+      case TABLE_PREFIX:
+        if(vtoken != IDENTIFIER){
+          printf("Syntax error in line %d, Expeted an identifier but found %s\n", yylineno, yytext);
+          return 1;
+        }
+        printf("%s is set to %s\n" yytext );
+        break;
+      case PORT:
+        if(vtoken != INTEGER){
+          printf("Syntax error in line %d, Expeted an identifier but found %s\n", yylineno, yytext);
+          return 1;
+        }
+        printf("%s is set to %s\n");
+        break;
+      default:
+        printf("Syntax error in line %d\n", yylineno);
+        scanf(i);
+    }
+    if(b==c==d==e){
+
+    }else if(d==e || e!=q &&){
+
+    }else{
+
+    }
+
+  }
+  
 }
